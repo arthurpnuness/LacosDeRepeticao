8333154060aff1a806c77177a165b131b4eeaa65
arthurpnuness <arthurpessoanunes@gmail.com>
2024-09-29 08:19:44 -0300

    up

---
+++ ex004.py
@@ -0,0 +1,6 @@
+'''Faça um programa que receba a idade e o peso de 7 pessoas, calcule e mostre:
+A média das idades das 7 pessoas;
+Quantidade de pessoas maiores de idade.
+Porcentagem de pessoas com idade entre 10 e 30 anos
+'''
+
